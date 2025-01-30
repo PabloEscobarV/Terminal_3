e639a674bc86ed7268294d6568b43d7a3a02a217
Pablo Escobar <sataniv.rider@gmail.com>
2025-01-30 20:53:16 +0100

    Started work on netdata

--- netdata/hdrs/netdata.h
+++ netdata/hdrs/netdata.h
@@ -6,24 +6,33 @@
 /*   By: Pablo Escobar <sataniv.rider@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 23:27:48 by Pablo Escob       #+#    #+#             */
-/*   Updated: 2024/12/09 23:36:34 by Pablo Escob      ###   ########.fr       */
+/*   Updated: 2025/01/30 20:49:21 by Pablo Escob      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef NETDATA_H
 #define NETDATA_H
 
+# define ND_INFILE_RDR	"<"
+# define ND_OUTFILE_RDR	">"
+# define ND_APPEND_RDR	">>"
+# define ND_HERDOC_RDR	"<<"
+
 typedef enum	e_operations
 {
-	OR,
-	PIPE,
-	AND
+	E_OPER_PIPE,
+	E_OPER_OR,
+	E_OPER_AND,
+	E_OPER_SIZE
 }							t_operations;
 
 typedef struct	s_argv
 {
-	char					*argv;
-	char					*path;
+	t_uchar				in_herdoc;
+	t_uchar				out_append;
+	char					*in_file;
+	char					*out_file;
+	char					**argv;
 	t_operations	operation;
 }								t_argv;
 
