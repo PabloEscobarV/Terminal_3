b1c7c0118510389193f682a72337463bfe8e207e
Pablo Escobar <sataniv.rider@gmail.com>
2025-02-16 19:57:36 +0100

    TERMINAL: Started work on netdata

--- netdata/src/netdata.c
+++ netdata/src/netdata.c
@@ -6,7 +6,7 @@
 /*   By: Pablo Escobar <sataniv.rider@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 23:32:36 by Pablo Escob       #+#    #+#             */
-/*   Updated: 2025/02/09 19:53:38 by Pablo Escob      ###   ########.fr       */
+/*   Updated: 2025/02/16 19:56:40 by Pablo Escob      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,33 @@
 #include "../../splitter/hdrs/splitter.h"
 #include "../../libft/libft.h"
 
-static const char	*_cmp_str_ch(const char ch, const char *strv)
+static t_cchar	*_cmp_str_ret_crd(t_cchar *str, t_cchar **strv)
 {
-	while (*strv && ch != *strv)
+	while (*strv && !ft_strlcmp(str, *strv))
 		++strv;
 	return (strv);
 }
 
-static void	_skip_qts(t_cchar *data, t_crd *crd, t_cchar qts)
+static t_uchar	*_get_oper(t_cchar *args, t_crd *crd, t_cchar **data,
+				t_cchar **splt)
 {
-	while (crd->i < crd->size && data[crd->i] != qts)
-		++crd->i;
-}
+	t_cchar	*operation;
 
-static void	_find_end_fname(t_cchar *data, t_crd *crd, t_cchar *rsrvd)
-{
-	while (crd->i < crd->size && !(*_cmp_str_ch(data[crd->i], rsrvd)) && !ft_isspace(data[crd->i]))
-		++crd->i;
-}
-
-static char	*_crdt_file_name(t_cchar *data, int i, t_crd *crd, char qts)
-{
-	char	*file_name;
-
-	if (qts)
-		++i;
-	file_name = malloc((crd->i - i + 1) * sizeof(char));
-	ft_strncpy(file_name, data + i, crd->i - i);
-	return (file_name);
-}
-
-char	*_get_file_name(t_cchar *data, t_crd *crd, t_rsrvd *rsrvd)
-{
-	int	i;
-	char	*qts;
-
-	i = crd->i;
-	qts = _cmp_str_ch(data + crd->i, rsrvd->qts);
-	if (*qts)
+	crd->i += ft_strlen(data);
+	while (crd->i < crd->size)
 	{
+		operation = cmp_str_ret_crd(args + crd->i, splt);
+		if (operation)
+			break ;
 		++crd->i;
-		_skip_qts(data, crd->i, *qts);
 	}
-	else
-		_find_end_fname(data, crd, rsrvd->reserved);
-	return (_crdt_file_name(data, i, crd, *qts));
+	crd->i += ft_strlen(operation);
+	return (operation);
 }
 
-t_uchar	_set_argvt_data(const char *data, t_argv *argvt)
+t_cchar	_get_oper_code(t_cchar *operation)
 {
-	while (*data)
-	{
-		
-	}
-}
-
-t_argv	*_set_argvt()
-{
-
+	
 }
 
 t_argv	*netdata(const char *args, const char **data, t_splt *splt)
